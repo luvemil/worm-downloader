0097cc47136001c22296cdf7b3b828cef2e3fa1d
luvemil <taran.marco@gmail.com>
2014-11-01 18:07:12 +0100

    Add simple error control to download_chaps.rb

--- download_chaps.rb
+++ download_chaps.rb
@@ -3,14 +3,36 @@ require 'net/http'
 require 'nokogiri'
 
 url = 'parahumans.wordpress.com' 
+article_tag = "//div[@class='entry-content']"
 
 paths = ObjectStash.load 'files/paths.stash'
 
+err = []
 
 paths.each do |path|
-  source = Net::HTTP.get(url,path)
-  doc = Nokogiri::HTML(source)
-  n = path.split('/').size
-  ObjectStash.store doc.xpath("//div[@class='entry-content']")[0].content.to_s, "files/#{path.split('/')[n]}.stash"
+  # Get a string to name a chapter from its path
+  path_splitted = path.split('/')
+  n = path_splitted.size - 1 
+  filename = "files/chapters/#{path_splitted[n]}.stash"
+
+  unless File::exists? filename
+    # Get the html and transform into a NodeSet
+    source = Net::HTTP.get url, path
+    doc = Nokogiri::HTML source
+
+    # Parse the NodeSet
+    chapter_node = doc.xpath(article_tag)[0]
+
+    if chapter_node
+      ObjectStash.store chapter_node.content.to_s, filename
+    else
+      File::new "files/err/#{path_splitted[n]}", "w"
+      err << path
+    end
+      
+  end
+
+  ObjectStash.store err, "files/err.stash"
+
 end
 
