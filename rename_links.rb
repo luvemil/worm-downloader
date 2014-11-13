3639e3bbd88501e2b9941d9d0b084d76e544354a
luvemil <taran.marco@gmail.com>
2014-11-13 11:21:13 +0100

    Remove old files

--- rename_links.rb
+++
@@ -1,9 +0,0 @@
-require './stash'
-
-links = ObjectStash.load "files/chapters_urls.stash"
-
-url = 'parahumans.wordpress.com' 
-
-paths = links.map { |link| link.gsub(url,'').gsub('http://','') }
-
-ObjectStash.store paths, "files/paths.stash"
