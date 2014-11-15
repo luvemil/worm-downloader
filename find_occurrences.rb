bcb74463900fdd612599f7dd42be442e31ce51f8
jiiam <jiiam@gmx.com>
2014-11-16 00:05:56 +0100

    Add stuff to find_occurrences.rb

--- find_occurrences.rb
+++ find_occurrences.rb
@@ -12,11 +12,34 @@ html_files = base_names.pathmap("#{Worm::CHAPTERS_HTML_HOLDER}/%n.html")
 
 def do_stuff(html)
   doc = Nokogiri::HTML IO.read(html)
-  return doc.css("div.entry-content").css("br").size
+  return doc.css("div.entry-content").css("p").size
 end
 
+def do_string(html)
+  doc = Nokogiri::HTML IO.read(html)
+  string = ""
+  doc.css("div.entry-content").css("p").each do |key|
+    string = "#{string}, #{key['style']}" if key['style']
+  end
+  return string
+end
+
+def do_classes(html)
+  doc = Nokogiri::HTML IO.read(html)
+  arr = []
+  doc.css("div.entry-content").css("p").each do |key|
+    if key['style'] 
+      arr << key['style'] unless arr.include?(key['style'])
+    end
+  end
+  return arr
+end 
+
+arr = []
 html_files.each do |file|
-  puts "There are #{do_stuff(file)} <p></p> tag enclosures in #{file}" if File.exists?(file)
+  #puts "Found: #{do_string(file)} in #{file}" if File.exists?(file)
+  arr += do_classes(file)
 end
 
+puts arr.uniq
 
