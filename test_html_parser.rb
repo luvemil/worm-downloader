0ac6f436d6b15256612c56d8deae2cc0104ac855
luvemil <taran.marco@gmail.com>
2014-11-16 19:19:06 +0100

    Non working version of test_html_parser.rb

--- test_html_parser.rb
+++ test_html_parser.rb
@@ -28,7 +28,58 @@ NONP_TAGS = %W[span li ul h3 div em address strong blockquote]
 P_TAGS = %W[em br del strong span i b]
 NONP_ONLY_TAGS = NONP_TAGS - P_TAGS 
 
+
+# Find everything from a given root node: tags, attributes and attribute
+# values for each children
+# return hash = { tag => { attribute => [ values ] } }
+def attribute_values root, tags
+  stuff = {}
+  tags.each do |tag| 
+    stuff[tag] = {} 
+    root.css(tag).each do |node|
+      node.attributes.each do |attribute|
+        stuff[tag][attribute] = [] unless stuff[node.name][attribute]
+        stuff[tag][attribute] = 
+          (stuff[tag][attribute] + [ node[attribute] ]).uniq
+      end
+    end
+  end
+  return stuff
+end
+
+# merges hashes in the form { attribute => [values] }
+def merge_hash first, second
+  first.merge(second) do |key, first_array, second_array| 
+    (first_array + second_array).uniq
+  end
+end
+# Prints on screen each tag
+def show_attributes values
+  values.keys.each do |key|
+    puts "#{key}:"
+    values[key].keys.each do |attribute|
+      puts "\t#{attribute}"
+      values[key][attribute].each do |value|
+        puts "\t\t#{value}"
+      end
+    end
+  end
+end
+
+def find_attribute_values html_files
+  values = {}
+  html_files.each do |html|
+    doc = Nokogiri::HTML IO.read(html)
+    doc.css("div.entry-content").each do |root|
+      values = values.merge(attribute_values(root, P_TAGS)) {|key, a, b| merge_hash(a,b)}
+    end
+  end
+  show_attributes values
+  return values
+end 
+
 def main html_files
+  find_attribute_values html_files
 end
 
 main html_files
