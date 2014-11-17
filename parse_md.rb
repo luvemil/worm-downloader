a8ffd279757fd15aeec347cb145fb1e2c1efbf15
luvemil <taran.marco@gmail.com>
2014-11-17 14:52:00 +0100

    Begin a parser html -> md in worm.rb

--- parse_md.rb
+++ parse_md.rb
@@ -13,6 +13,21 @@ def parse node
   return node.text
 end
 
+def show_me node
+  strings = ""
+  node.children.each do |child|
+    if child.name == "em"
+      strings += "*#{show_me(child)}*"
+    elsif child.name == "strong"
+      strings += "_#{show_me(child)}_"
+    elsif child.name == "text"
+      strings += child.text
+      return strings
+    end
+  end
+  return strings
+end
+
 puts "\##{doc.css(TITLE_TAG).text}\n"
 root.css("p").each do |node|
   puts parse(node) unless node.css("a").size > 0
