c485164b327a47fc168d1ca7b0f2a93f0b8fdc1d
luvemil <taran.marco@gmail.com>
2014-11-17 15:48:06 +0100

    First example of parsing
    run parse_md.rb html_file > md_file

--- worm.rb
+++ worm.rb
@@ -27,6 +27,11 @@ module Worm
     return strings
   end 
 
+  def self.show_text node
+    # Here we do some basic escape to avoid issues
+    return node.text.gsub('*','\*')
+  end
+
   def self.convert child
     # Converts an html tag to the markdown equivalent, returns a string.
     # Assumes the input is a well formatted HTML snippet, i.e. no
@@ -37,10 +42,71 @@ module Worm
     elsif tag == "strong"
       return  "**#{self.show_me(child)}**"
     elsif tag == "text"
-      return child.text
+      return self.show_text(child)
+    elsif tag == "br"
+      return "#{self.show_me(child)}\\"
+    elsif tag == "p"
+      return "#{self.show_me(child)}\n\n"
     end
-    return child.text
+    return self.show_text(child)
   end
 
+  def self.parse root
+    root.css("p").each do |node|
+      print self.convert(node) unless node.css("a").size > 0
+    end
+  end
+
+  # All the attributes I found
+  ATTRIBUTES = {"p" => {
+    "style" => %W[ 
+                "text-align:right;"
+                "text-align:center;"
+                "padding-left:30px;"
+                "text-align:left;"
+                "text-align:left;padding-left:30px;"
+                "padding-left:30px;text-align:center;"
+                "text-align:center;padding-left:30px;"
+                "padding-left:60px;" ],
+                "dir" => %W[
+                "ltr" ],
+                  "id" => %W[
+                "internal-source-marker_0.1750084241491393"
+                "firstHeading" ],
+                  "align" => %W[
+                "LEFT"
+                "CENTER"
+                "center" ]
+  },
+  "span" => {
+    "style" => %W[
+                "text-decoration:underline;"
+                "line-height:15px;"
+                "color:#333333;font-style:normal;line-height:24px;"
+                "font-style:inherit;line-height:1.625;"
+                "font-size:15px;font-style:inherit;line-height:1.625;"
+                "color:#ffffff;" ],
+                  "id" => %W[
+                "internal-source-marker_0.43876651558093727" ],
+                  "class" => %W[
+                "wp-smiley wp-emoji wp-emoji-wink"
+                "wp-smiley wp-emoji wp-emoji-sad"
+                "wp-smiley wp-emoji wp-emoji-mindblown" ],
+                  "title" => %W[
+                ";)"
+                ":("
+                "O_o" ]
+  },
+  "em" => {
+    "style" => %W[
+                "text-align:left;"
+                "line-height:1.625;"
+                "font-size:15px;line-height:1.625;" ]
+  },
+  "strong" => {
+    "style" => %W[
+                "padding-left:30px;" ]
+  }
+  }
 end
  
