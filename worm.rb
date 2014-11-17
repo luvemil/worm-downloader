65be398eb7881ca60766fe86294d5e4a3b2901ed
luvemil <taran.marco@gmail.com>
2014-11-17 14:52:00 +0100

    Begin a parser html -> md in worm.rb

--- worm.rb
+++ worm.rb
@@ -15,5 +15,32 @@ module Worm
     puts "Download #{path}"
     return Net::HTTP.get URL, path
   end
+
+  def self.show_me node
+    strings = ""
+    node.children.each do |child|
+      if child.text == ""
+      else 
+        strings += self.convert(child)
+      end
+    end
+    return strings
+  end 
+
+  def self.convert child
+    # Converts an html tag to the markdown equivalent, returns a string.
+    # Assumes the input is a well formatted HTML snippet, i.e. no
+    # nested <strong> or <em>
+    tag = child.name
+    if tag == "em"
+      return "_#{self.show_me(child)}_"
+    elsif tag == "strong"
+      return  "**#{self.show_me(child)}**"
+    elsif tag == "text"
+      return child.text
+    end
+    return child.text
+  end
+
 end
  
