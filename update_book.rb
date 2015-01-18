2dae22aed5c86ef99f168abf74970f61456449e6
luvemil <taran.marco@gmail.com>
2015-01-18 11:19:55 +0100

    Add documenting comments to new code

--- update_book.rb
+++ update_book.rb
@@ -10,6 +10,7 @@ require './worm'
 chapters = Marshal.load IO.read(Worm::CHAPTERS)
 names = chapters[:names]
 
+# BEGIN - putting in Book.txt just the chapters we want.
 # Get the start and end chapter from the arguments (if valid)
 if ARGV.length >= 2
   start_chap = ARGV[0]
@@ -20,9 +21,12 @@ else
 end
 
 if names.include?(start_chap) and names.include?(end_chap)
+  # Something might get ugly if we run something like `ruby update_book.rb
+  # chapter3 chapter1`?
   range = names.index(start_chap)..(names.index(end_chap))
   names = names[range]
 end
+# END
 
 # TODO: make a premade preface.md to put between tableofcontents and
 # mainmatter
